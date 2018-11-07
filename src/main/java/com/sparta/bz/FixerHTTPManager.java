482618f545106c1bc85fb873f9117bf976475f3c
Boris Zekic <bzekic@spartaglobal.com>
2018-11-07 13:46:43 +0000

    Complete fixer http for latest rates

---
+++ src/main/java/com/sparta/bz/FixerHTTPManager.java
@@ -0,0 +1,35 @@
+package com.sparta.bz;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class FixerHTTPManager {
+    private String base_url;
+    private String api_key;
+    private String latestRates;
+
+    public FixerHTTPManager() {
+        KeyReader keyReader = new KeyReader();
+        api_key = "access_key=" + keyReader.getApi_key();
+        base_url = "http://data.fixer.io/api";
+    }
+
+    public void setLatestRates() {
+        try {
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpGet getLatestRates = new HttpGet( base_url + "/latest?" + api_key );
+            CloseableHttpResponse response = httpClient.execute( getLatestRates );
+            latestRates = EntityUtils.toString(response.getEntity());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String getLatestRates() {
+        return latestRates;
+    }
+}
--- src/main/java/com/sparta/bz/Starter.java
+++ src/main/java/com/sparta/bz/Starter.java
@@ -2,8 +2,7 @@ package com.sparta.bz;
 
 public class Starter
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
 
     }
 }
